a0e4a57d3ed19c2bee7c644f8f2d97184ce42721
rprabusundaram <rprabusundaram@gmail.com>
2024-09-22 17:18:40 -0500

    tf cloud

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "my-ec2" {
   ami           = var.ec2_ami
   instance_type = var.ec2_type
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
-  user_data = file("userdata.sh")
+ 
   #key_name = var.ec2_keyname
   tags = {
     Name = "My-EC2"
